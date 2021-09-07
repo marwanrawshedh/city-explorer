fb93dcfcbff3aa4bb7c6edba72763c9dfe5ef714
marwanrawshedh <marwansalem55@yahoo.com>
2021-09-07 23:57:49 +0300

    labb8

---
+++ src/component/Movies.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Movies extends React.Component {
+    render() {
+        return (
+            <>
+             {this.props.Movies}   
+            </>
+
+        )
+    }
+}
+export default Movies;
